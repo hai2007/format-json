729e25274fe71808cf6be696c8d08c8fdbdca1b4
你好2007 <2501482523@qq.com>
2021-10-26 23:13:49 +0800

    简单的json字符串格式化工具

---
+++ README.md
@@ -0,0 +1,27 @@
+# [format-json](https://hai2007.github.io/format-json/)
+格式化json字符串的在线工具
+
+<img src='./view.png'>
+
+## Issues
+使用的时候遇到任何问题或有好的建议，请点击进入[issue](https://github.com/hai2007/format-json/issues)，欢迎参与维护！
+
+## How to use?
+
+开发的时候运行下列命令：
+
+```
+npm run dev
+```
+
+开发完毕需要发布的时候，请运行下列命令：
+
+```
+npm run build
+```
+
+开源协议
+---------------------------------------
+[MIT](https://github.com/hai2007/format-json/blob/master/LICENSE)
+
+Copyright (c) 2021 [hai2007](https://hai2007.gitee.io/sweethome/) 走一步，再走一步。
---
+++ src/App/index.ts
@@ -0,0 +1,91 @@
+import { Component } from 'nefbl'
+import { toJSON } from '@hai2007/algorithm/json.js'
+
+let OpenWebEditor = require('open-web-editor')
+
+
+import style from './index.scss'
+import template from './index.html'
+
+@Component({
+    selector: "app-root",
+    template,
+    styles: [style]
+})
+export default class {
+
+    $mounted() {
+
+        let target = new OpenWebEditor({
+            el: document.getElementById('target'),
+            color: {
+                background: "#eeeeee", /*编辑器背景*/
+                text: "#170", /*文本颜色*/
+                number: "#888484", /*行号颜色*/
+                edit: "#eaeaf1", /*编辑行背景色*/
+                cursor: "#ff0000", /*光标颜色*/
+                select: "#6c6cf1", /*选择背景*/
+            },
+            content: "",
+            shader: ["javascript", {
+                "text": "#000000",/*文本颜色*/
+                "annotation": "#ff0000",/*注释颜色*/
+                "insign": "#555555",/*符号颜色*/
+                "key": "#ff0000",/*关键字颜色*/
+                "string": "#ac4c1e",/*字符串颜色*/
+                "funName": "#1e50b3",/*函数名称颜色*/
+                "execName": "#1e83b1"/*执行方法颜色*/
+            }],
+            readonly: true
+        })
+
+        new OpenWebEditor({
+            el: document.getElementById('source'),
+            color: {
+                background: "#ffffff", /*编辑器背景*/
+                text: "#170", /*文本颜色*/
+                number: "#888484", /*行号颜色*/
+                edit: "#eaeaf1", /*编辑行背景色*/
+                cursor: "#ff0000", /*光标颜色*/
+                select: "#6c6cf1", /*选择背景*/
+            },
+            content: `/*
+请直接在此编辑或者粘贴你需要格式化的json字符串即可，
+支持非严格模式，例如：
+
+{
+    key:'value'
+}
+
+由任何问题，直接提issue： https://github.com/hai2007/format-json/issues
+
+本项目基于：
+【编辑器】https://github.com/hai2007/Open-Web-Editor
+【算法支持】https://github.com/hai2007/algorithm.js
+*/
+`,
+            shader: ["javascript", {
+                "text": "#000000",/*文本颜色*/
+                "annotation": "#6a9955",/*注释颜色*/
+                "insign": "#555555",/*符号颜色*/
+                "key": "#ff0000",/*关键字颜色*/
+                "string": "#ac4c1e",/*字符串颜色*/
+                "funName": "#1e50b3",/*函数名称颜色*/
+                "execName": "#1e83b1"/*执行方法颜色*/
+            }]
+        }).updated(function () {
+
+            try {
+                target.valueOf(JSON.stringify(toJSON(this.valueOf()), null, 2))
+            } catch (e) {
+                target.valueOf("/*\n【发生了错误】\n" + e + "\n*/")
+            }
+
+            // 使得光标回到录入编辑器界面
+            document.getElementById('source').click()
+
+        })
+
+    }
+
+}
